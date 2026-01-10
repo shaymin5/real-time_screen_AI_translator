88163633be0f7074b49f8e7ef472acebd30aab3e
shaymin5 <2767272116@qq.com>
2026-01-10 14:57:29 +0800

    perf: check增加长度限制。

--- checker.py
+++ checker.py
@@ -4,16 +4,19 @@ from collections import deque
 class Checker:
     """
         检查OCR传过来的文本是否需要翻译和体现。
-        目前方案：检测文本是否与之前的发生明显变化。无论结果如何都存储新文本（不存储空文本）作为下一次判断的依据。
+        目前方案：1、检测文本是否过长；2、检测文本是否与之前的发生明显变化。无论结果如何都存储新文本（不存储空文本）作为下一次判断的依据。
     """
-    def __init__(self, queue_size: int = 2, similarity: float = 0.97) -> None:
+    def __init__(self, queue_size: int = 2, similarity: float = 0.8) -> None:
         self.queue = deque([''],maxlen=queue_size)
-        self.similarity = similarity # 文本相似度阈值，低于该阈值则认为发生明显变化。越高越严格。
+        self.similarity = similarity # 文本相似度阈值，低于该阈值则认为发生明显变化。越低说明变化需要越明显才能通过。
 
-    def check(self, new_text: str) -> bool:
+    def check(self, new_text: str, maxlen: int = 200) -> bool:
         """
-            检测新文本是否与之前的历史文本发生明显变化。如发生明显变化则返回True。
+        检测文本长度是否过长。
+        检测新文本是否与之前的历史文本发生明显变化。如发生明显变化则返回True。
         """
+        if len(new_text) > maxlen:
+            return False
         if new_text == '':
             return False
         if self.queue[-1] == '':
@@ -24,6 +27,7 @@ class Checker:
             # 计算当前文本与历史文本的相似度
             similarity = Levenshtein.ratio(self.queue[-1], self.queue[-2])
             if similarity < self.similarity:
+                # 如果很不相似，则通过检查
                 return True
             else:
                 return False
