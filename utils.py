f0b2e59417caac9698615502806791ff38a1489c
shaymin5 <2767272116@qq.com>
2026-01-27 19:45:52 +0800

    del: 删除过时类

--- utils.py
+++ utils.py
@@ -9,7 +9,7 @@ class Checker:
         检查OCR传过来的文本是否需要翻译和体现。
         目前方案：1、检测文本是否过长；2、检测文本是否与之前的发生明显变化。无论结果如何都存储新文本（不存储空文本）作为下一次判断的依据。
     """
-    def __init__(self, queue_size: int = 2, similarity: float = 0.8) -> None:
+    def __init__(self, queue_size: int = 2, similarity: float = 0.95) -> None:
         self.queue = deque([''],maxlen=queue_size)
         self.similarity = similarity # 文本相似度阈值，低于该阈值则认为发生明显变化。越低说明变化需要越明显才能通过。
 
@@ -34,32 +34,6 @@ class Checker:
                 return True
             else:
                 return False
-            
-
-class FadeQueue:
-    def __init__(self, maxsize: int = 10, timeout: float = 3) -> None:
-        self.queue = queue.Queue(maxsize=maxsize)
-        self.timeout = timeout
-    def put(self, item: Any):
-        try:
-            self.queue.put(item,timeout=self.timeout)
-        except queue.Full:
-            while self.queue.full():
-                try:
-                    self.queue.get_nowait()
-                    self.queue.put(item,timeout=self.timeout)
-                except queue.Full:
-                    pass
-                except queue.Empty:
-                    self.queue.put(item,timeout=self.timeout)
-    def get(self) -> Any:
-        return self.queue.get()
-    def empty(self) -> bool:
-        return self.queue.empty()
-    def get_nowait(self) -> Any:
-        return self.queue.get_nowait()
-    def put_nowait(self, item: Any):
-        return self.queue.put_nowait(item)
     
 
 
